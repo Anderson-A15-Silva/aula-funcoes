1ebaa8c5c1f64759590cd4820f9f5651ee4b92c4
Anderson A. Silva <andersonadsilva15@gmail.com>
2023-08-24 21:56:06 -0300

    Adicionando index.html e index.js no repositório

---
+++ index.js
@@ -0,0 +1,38 @@
+// Exercícios de interpretação de código
+
+/*
+Exercício 1
+Resposta A:
+10, 50
+Resposta B:
+Como o valor dentro da função não foi colocado dentro do 'console.log' ele não exibiria na página da web.
+*/
+
+/*
+Exercício 2
+Resposta A:
+Pede que o usuário digite um texto, posteriormente, dentro da função o texto altera é alterado para minúsculo
+e o "programa" procura pela palavra 'cenoura', exibindo a resposta do usuário com as mudanças na página da web.
+Resposta B:
+I.
+eu gosto de cenoura, true
+II.
+cenoura é bom pra vista, true
+III.
+cenouras crescem na terra, false
+*/
+
+// Exercícios de escrita de código
+
+/*
+Exercício 1 (Incompleto)
+Resposta A:
+
+const exercicioUsuario = () => {
+    const informacoesUsuario = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+    return informacoesUsuario
+}
+
+const resposta = exercicioUsuario
+console.log(resposta)
+*/
